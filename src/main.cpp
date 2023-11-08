84730acd27a5c8b69dbc46db7cbf450735e35f3a
RAximberdi <r.raxonberdiyev@newuu.uz>
2023-11-08 11:37:08 +0500

    first

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ int main() {
     // call for task 3
     std::cout << "Task 4" << std::endl;
     // call for task 4
-    std::cout << "Task 5ddfds" << std::endl;
+    std::cout << "Task 5ddfdsew" << std::endl;
     // call for task 5
     return 0;
 }

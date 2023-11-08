68b94ef98b0072abcd46c7642e6aa0bae1c22da8
RAximberdi <r.raxonberdiyev@newuu.uz>
2023-11-08 11:36:03 +0500

    first

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ int main() {
     // call for task 3
     std::cout << "Task 4" << std::endl;
     // call for task 4
-    std::cout << "Task 5" << std::endl;
+    std::cout << "Task 5ddfds" << std::endl;
     // call for task 5
     return 0;
 }
--- src/task_1.h
+++ src/task_1.h
@@ -3,3 +3,35 @@
  * Date:
  * Name:
  */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool prime(long long a)
+{
+    if(a > 3)
+        for(int j=2; j<=sqrt(a); j++)
+        {
+            if(!(a%j))
+                return true;
+        }
+    return false;
+}
+
+int main()
+{
+    long long n;
+    cin >> n;
+    for(int i=1; i<=10000; i++)
+    {
+        if(prime(i*n+1))
+        {
+            cout << i;
+            return 0;
+        }
+    }
+
+
+
+    return 0;
+}
